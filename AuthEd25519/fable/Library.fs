cf7ef86b3ada0ab35f78276eb67bf8c9be6e46c4
vankata <vankata@pop-os.localdomain>
2021-09-14 20:00:45 +0300

    TezFs update

--- AuthEd25519/fable/Library.fs
+++ AuthEd25519/fable/Library.fs
@@ -1,71 +1,53 @@
-﻿namespace AuthEd25519
+﻿module AuthEd25519
 
 
 open System
 open System.Diagnostics
 open System.Security.Cryptography
 open System.Text
-open Chaos.NaCl
 open Konscious.Security.Cryptography
 open Newtonsoft.Json
-module AuthEd25519 =
-[<JsonObject(MemberSerialization = MemberSerialization.Fields)>]
-type PrivateKey = private PrivateKey of byte [] with
-    member this.string = match this with PrivateKey p -> Convert.ToBase64String p
-    member this.bytes = match this with PrivateKey p -> p |> Array.copy
-[<JsonObject(MemberSerialization = MemberSerialization.Fields)>]
-type PublicKey = private PublicKey of byte [] with
-    member this.string = match this with PublicKey p -> Convert.ToBase64String p
-    member this.bytes = match this with PublicKey p -> p |> Array.copy
-[<JsonObject(MemberSerialization = MemberSerialization.Fields)>]
-type Signature = private Signature of byte [] with
-    member this.string = match this with Signature p -> Convert.ToBase64String p
-    member this.bytes = match this with Signature p -> p |> Array.copy
-[<JsonObject(MemberSerialization = MemberSerialization.Fields)>]
-type Seed = private Seed of byte [] with
-    member this.string = match this with Seed p -> Convert.ToBase64String p
-    member this.bytes = match this with Seed p -> p |> Array.copy
+//module AuthEd25519 =
+open TezFs open Netezos open Netezos.Keys
 
-let rand = RNGCryptoServiceProvider.Create()
-let empty_seed() = Seed <| Array.init 32 (fun _ -> 0uy)
+[<JsonObject(MemberSerialization = MemberSerialization.Fields)>]
+type PrivateKey = private PrivateKey of Key with
+    member this.string = match this with PrivateKey p -> p.GetBase58()
+    member this.bytes = match this with PrivateKey p -> p.GetBytes()
+[<JsonObject(MemberSerialization = MemberSerialization.Fields)>]
+type PublicKey = private PublicKey of PubKey with
+    member this.string = match this with PublicKey p -> p.GetBase58()
+    member this.bytes = match this with PublicKey p -> p.GetBytes()
+[<JsonObject(MemberSerialization = MemberSerialization.Fields)>]
+type Signature = private Signature of Netezos.Keys.Signature with
+    member this.string = match this with Signature p -> p.ToBase58()
+    member this.bytes = match this with Signature p -> p.ToBytes()
 
-let bpub, bpriv, bseed = PublicKey, PrivateKey, Seed
-let spub (s:string) = Convert.FromBase64String s |> bpub
-let spriv (s:string) = Convert.FromBase64String s |> bpriv
-let sseed (s:string) = Convert.FromBase64String s |> bseed
 
-let load_pub_priv_from_strings pub priv = spub pub, spriv priv
-let load_pub_priv_from_seed (Seed seed) =
-    let pub, priv = ref (Array.init 32 (fun _ -> 0uy)), ref <| Array.init 64 (fun _ -> 0uy)
-    Ed25519.KeyPairFromSeed (pub, priv, seed)
-    PublicKey !pub, PrivateKey !priv
-let mk_seed () = let seed = empty_seed().bytes in let _ = rand.GetBytes seed in Seed seed
-let mk_seed_pub_priv () = let seed = mk_seed() in seed, load_pub_priv_from_seed seed
+let spriv (s:string) = acc_base58 s
+
 let mk_sign ps (msg:string) =
     let sigs = ps |> List.map (fun  (PrivateKey p) ->
-        let data = Encoding.UTF8.GetBytes msg
-        let signature = Ed25519.Sign(data, p)
+        let signature = p.Sign(msg)
         Signature signature)
     msg, sigs
 let mk_verifier (ps:PublicKey list) (msg:string, signatures) =
-    let data = Encoding.UTF8.GetBytes msg
-    if ps |> List.exists (fun (PublicKey p) -> signatures |> List.exists (fun (Signature signature) -> Ed25519.Verify(signature, data, p)))
+    if ps |> List.exists (fun (PublicKey p) -> signatures |> List.exists (fun (Signature signature) -> p.Verify(msg, signature.ToBase58())))
     then Ok msg
     else Error <| sprintf "Signature Verification Failed: pubs: %A; msg: %s;" ps msg
 let combine_signatures (t, ss) (t2, ss2) = if t = t2 then t, List.append ss ss2 else t, ss
-type Account = {pub: PublicKey; priv: PrivateKey;}
+[<JsonObject(MemberSerialization = MemberSerialization.Fields)>]
+type Account = private {priv: PrivateKey;}
     with
+    member acc.pub = match acc.priv with PrivateKey p -> PublicKey p.PubKey
+    member acc.pub_hash = match acc.priv with PrivateKey p -> p.PubKey.Address
     member acc.sign = mk_sign [acc.priv]
-let new_account () = let seed, (pub, priv) = mk_seed_pub_priv () in seed, {pub = pub; priv = priv}
-let bload_account pub priv = {pub=pub; priv=priv}
-let sload_account pub priv = bload_account <|| load_pub_priv_from_strings pub priv
-let bload_account_seed seed = load_pub_priv_from_seed seed ||> bload_account
-let sload_account_seed seed = sseed seed |> load_pub_priv_from_seed ||> bload_account
+let new_account () = random_account ()
 
 let argon2hash iterations memory_size degrees_of_parallelism size known_secret extra salt pass =
-    let arg = Argon2id pass
+    let arg = new Argon2id(pass)
     arg.Iterations <- iterations
-    arg.Salt
+    arg.Salt <- salt
     arg.AssociatedData <- extra
     arg.KnownSecret <- known_secret
     arg.MemorySize <- memory_size
@@ -96,7 +78,8 @@ let shasherino known_secret (extra:string) (salt:string) (pass:string) =
     let m = int32 <| harden 100_000L def_memory_size + weakness
     let d = int32 <| def_degree_of_parallelism
     argon2hash its m d 32 known_secret salt extra pass
-let load_acc_from_details known_secret extra salt pass = shasherino known_secret extra salt pass |> bseed |> fun s -> s, bload_account_seed s
+let load_acc_from_details known_secret extra salt pass = shasherino known_secret extra salt pass |> Key.FromBytes |> fun k -> {priv = PrivateKey k}
+let memory_login name pass = load_acc_from_details "be5d9f66cf2ebed67f5a3c68ce69bd07a1ecad2b88db7cfaf5ac63aba8eaf6fe" "hog"
 let bench f =
     let t = Stopwatch.StartNew()
     let ret = f()
